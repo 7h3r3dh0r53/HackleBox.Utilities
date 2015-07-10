debfc1fe9dc23a768387e546bbb8f119c55781dd
TheHorseman <thehorseman@localhost.localdomain>
2015-07-10 18:41:49 -0400

    Cleanup

---
+++ Extensions/Conversion.cs
@@ -0,0 +1,18 @@
+namespace HackleBox.Utilities
+{
+    public static class Conversion
+    {
+        
+        public static int AsIntOrDefault(this string s, int Default = 0)
+        {
+            return (int)s.AsIntOrNull(Default);
+        }
+        public static int? AsIntOrNull(this string s, int? Default = null)
+        {
+            int parsed;
+            if(int.TryParse(s, out parsed))
+                Default = parsed;
+            return Default;
+        }
+    }
+}
